9dc0d892e09a4495c79ba723d13088ff637f8aa6
Clovebooke <christlovephoebe98@gmail.com>
2025-05-10 16:05:19 +0100

    cse 210 exercise 2 new commit

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please enter your grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+        int percentage = int.Parse(gradePercentage);
+
+        string grade = "";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
     }
 }
